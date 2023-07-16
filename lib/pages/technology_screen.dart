c6ca1e5022dbd5cdad917e62dfe0e512c68d7744
JJaguar657 <97426855+JJaguar657@users.noreply.github.com>
2023-07-16 10:28:19 +0530

    Update technology_screen.dart

--- lib/pages/technology_screen.dart
+++ lib/pages/technology_screen.dart
@@ -59,7 +59,7 @@ class _TechnologyScreenState extends State<TechnologyScreen> {
                           child: Image.network(
                             news[index].urlToImage,
                             fit: BoxFit.cover,
-                            height: 400.0,
+                            height: 300.0,
                           ),
                         ),
                       ),
@@ -79,7 +79,7 @@ class _TechnologyScreenState extends State<TechnologyScreen> {
                                   child: Text(
                                     news[index].title,
                                     style: const TextStyle(
-                                      fontSize: 12,
+                                      fontSize: 14,
                                       fontWeight: FontWeight.bold,
                                     ),
                                   ),
